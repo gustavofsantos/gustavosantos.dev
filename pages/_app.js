b1d5372f6dc92b85e35f46c972bb8c88c1ecb0a2
Gustavo Santos <gustavo@after.sale>
2020-09-17 21:28:36 -0300

    Finish index page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import { Global, css } from "@emotion/core";
+import { ThemeProvider } from "emotion-theming";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return (
+    <main>
+      <Global
+        styles={css`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: "Inter", sans-serif;
+          }
+
+          * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+          }
+        `}
+      />
+      <Head>
+        <title>Gustavo Santos</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </main>
+  );
 }
-
-export default MyApp
