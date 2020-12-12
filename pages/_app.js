84e98ba04e76dd33d910aaf8514d650fdf28c1ea
Gustavo Santos <gustavofsantos@outlook.com>
2020-12-12 15:39:50 +0000

    replace old styling to use tailwindcss

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,13 @@
 import Head from "next/head";
-import { Global, css } from "@emotion/core";
-import { ThemeProvider } from "emotion-theming";
+import "../styles/global.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <main>
-      <Global
-        styles={css`
-          html,
-          body {
-            padding: 0;
-            margin: 0;
-            font-family: "Inter", sans-serif;
-          }
-
-          * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-          }
-        `}
-      />
+    <main className="h-full w-full">
       <Head>
         <title>Gustavo Santos</title>
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
       </Head>
--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,9 @@
-/** @jsx jsx */
-import { css, jsx } from "@emotion/core";
-import { Page, PageContent } from "../styles/Page";
+import { HomeHero } from "../components/home-hero";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <Page>
-      <div
-        css={css`
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          height: 100%;
-          width: 100%;
-        `}
-      >
-        <PageContent>
-          <h1
-            css={css`
-              font-size: 5em;
-              font-weight: 400;
-
-              @media (max-width: 800px) {
-                font-size: 3em;
-              }
-            `}
-          >
-            Hi! I'm Gustavo Santos. I turn ideas into JavaScript code.
-          </h1>
-          <h3
-            css={css`
-              font-weight: 400;
-            `}
-          >
-            Web developer at{" "}
-            <span
-              css={css`
-                font-weight: 700;
-              `}
-            >
-              aftersale
-            </span>
-          </h3>
-        </PageContent>
-      </div>
-    </Page>
+    <article className="flex flex-col w-full h-full justify-center items-center">
+      <HomeHero />
+    </article>
   );
 }
