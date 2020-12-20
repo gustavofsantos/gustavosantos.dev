a9a9dc572756fc375f1e5c1431f43fc40632f635
Gustavo Santos <gustavofsantos@outlook.com>
2020-12-20 13:03:31 -0300

    decrease home hero font size

--- components/home-hero.js
+++ components/home-hero.js
@@ -1,7 +1,7 @@
 export const HomeHero = () => (
   <section className="flex flex-col w-full p-6 lg:p-0 max-w-lg lg:max-w-2xl">
-    <h1 className="font-light w-full text-5xl lg:text-7xl text-black dark:text-gray-200">
-      Olá! Me chamo Gustavo Santos. Eventualmente transformo ideias em código.
+    <h1 className="font-light w-full text-4xl md:text-5xl lg:text-7xl text-black dark:text-gray-200">
+      Olá! Me chamo Gustavo Santos. Transformo ideias em código.
     </h1>
   </section>
 )
