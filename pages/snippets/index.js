e70ace427d27561ca6964348b6e134671b7b8176
Gustavo Santos <gustavofsantos@outlook.com>
2020-12-29 10:51:21 -0300

    created snippets page and link to the header navigation

---
+++ pages/snippets/index.js
@@ -0,0 +1,11 @@
+export default function SnippetsPage() {
+  return (
+    <article>
+      <section>
+        <h1>Snippets</h1>
+
+        <p>Um conjunto de trechos muito úteis de código</p>
+      </section>
+    </article>
+  )
+}
