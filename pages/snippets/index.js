b307b4f64a235172e1bef75f77ea897edd0bc03a
Gustavo Santos <gustavofsantos@outlook.com>
2020-12-29 12:05:14 -0300

    done first snippet

--- pages/snippets/index.js
+++ pages/snippets/index.js
@@ -1,11 +1,32 @@
-export default function SnippetsPage() {
-  return (
-    <article>
-      <section>
-        <h1>Snippets</h1>
+import { SnippetList } from '../../components/snippets/snippet-list'
+import { getAllSnippets } from '../../lib/snippets'
 
-        <p>Um conjunto de trechos muito úteis de código</p>
+export default function SnippetsPage({ snippets }) {
+  return (
+    <article className="flex flex-col items-center">
+      <section className="pt-20 pb-10 w-full pl-4 pr-4 md:max-w-lg lg:max-w-xl">
+        <h1 className="poppins text-3xl md:text-4xl lg:text-5xl font-bold text-center pb-6">
+          Snippets
+        </h1>
+        <h3 className="text-gray-700 text-lg">
+          An collection of code snippets, commands, config and tooling that
+          could be very useful
+        </h3>
       </section>
+
+      <div className="flex flex-col items-center pb-36 w-full">
+        <section className="flex flex-col w-full pl-4 pr-4 md:max-w-lg lg:max-w-xl md:p-0">
+          <SnippetList snippets={snippets} />
+        </section>
+      </div>
     </article>
   )
+}
+
+export async function getStaticProps() {
+  const snippets = await getAllSnippets()
+
+  return {
+    props: { snippets }
+  }
 }
