142bddbb09b944343bb8d084573633cf8d77ee27
Gustavo Santos <gustavofsantos@outlook.com>
2021-01-02 14:11:23 -0300

    wip

--- components/apps/app-card-shhh.js
+++ components/apps/app-card-shhh.js
@@ -3,7 +3,7 @@ import { AppCard } from './app-card'
 export const AppCardShhh = () => (
   <AppCard
     title="Shh"
-    description="Um app para compartilhar segredos de forma segura usando o mecanismo de derivação de chave. Nenhum dado é armazenado."
+    description="A secret web app to share secrets using key derivation scheme. No data is stored. Free to use."
     url="https://shhh.gustavosantos.dev"
     repositoryUrl="https://github.com/gustavofsantos/shhh"
     previewImagePath="/images/apps/previews/shhh.gustavosantos.dev_.png"
--- components/blog/blog-post-list.js
+++ components/blog/blog-post-list.js
@@ -1,7 +1,7 @@
 import { BlogPostCard } from './blog-post-card'
 
 export const BlogPostList = ({ posts }) => (
-  <section className="flex flex-col w-full pl-4 pr-4 md:max-w-lg lg:max-w-xl md:p-0 divide-y divide-gray-100 dark:divide-gray-800">
+  <section className="flex flex-col w-full pl-4 pr-4 md:max-w-lg lg:max-w-xl md:p-0 divide-y divide-gray-200 dark:divide-gray-800">
     {posts.map((post) => (
       <BlogPostCard
         key={post.href}
