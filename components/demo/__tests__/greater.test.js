9a77ca022ada13d360cbd3eab16b5084bbb87ec2
Gustavo Santos <gustavofsantos@outlook.com>
2021-01-31 13:58:08 -0300

    setup jest testing workflow

--- components/demo/__tests__/greater.test.js
+++ components/demo/__tests__/greater.test.js
@@ -1,10 +1,9 @@
 import { Greater } from '../greater'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 describe(Greater.name, () => {
   test('should great the name', () => {
     const name = 'George'
     render(<Greater name={name} />)
-    screen.debug()
   })
 })
